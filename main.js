9b44f660fc19d37085b911177963408ad3121660
eversunhuang <ever.sun.huang@gmail.com>
2026-02-06 11:16:34 -0800

    Screen 1 & 2 done

---
+++ main.js
@@ -0,0 +1,28 @@
+/*
+things to include:
+- movable candy hearts stickers
+- spotify playlist
+- digi camera animation
+- light up cat lamp
+- spinning gears
+
+sections:
+- home
+- where i'm from
+    - nyc: subway scroll animation
+    - china: open fortune cookies
+    - wa: rising moon over rainier
+
+- projects
+    - robots
+    
+
+- gallery
+    - digi camera
+
+
+*/
+
+function screen1Hover() {
+    this.setAttribute("background-color", "white");
+}
